db644fedea4facb2295c1b5fda6a77a5f90aa654
Krynnlord <richard.landon.miller@gmail.com>
2025-09-19 13:33:16 -0500

    Added Contacts App

---
+++ Contacts/contacts.py
@@ -0,0 +1,173 @@
+import os
+import json
+from datetime import datetime, timedelta
+import time
+import base64
+from cryptography.fernet import Fernet
+from hashlib import sha256
+
+VAULT_FILE = 'contacts.dat'
+
+def clear_screen():
+    os.system("cls" if os.name == "nt" else "clear")
+
+def derive_key(password: str) -> bytes:
+    return base64.urlsafe_b64encode(sha256(password.encode()).digest())
+
+def load_vault(fernet: Fernet) -> dict:
+    if not os.path.exists(VAULT_FILE):
+        return {}
+    with open(VAULT_FILE, 'rb') as f:
+        encrypted_data = f.read()
+    try:
+        decrypted_data = fernet.decrypt(encrypted_data)
+        return json.loads(decrypted_data)
+    except:
+        print("Invalid master password or corrupted vault.")
+        exit(1)
+
+def save_vault(data: dict, fernet: Fernet):
+    with open(VAULT_FILE, 'wb') as f:
+        encrypted_data = fernet.encrypt(json.dumps(data).encode())
+        f.write(encrypted_data)
+
+def add_entry(vault: dict):
+
+    site = input("\33[93mFirst Name: \33[0m")
+    last_name = input("\33[93mLast Name: \33[0m")
+    title = input("\33[93mTitle: \33[0m")
+    phone_number = input("\33[93mPhone Number: \33[0m")
+    email = input("\33[93mEmail: \33[0m")
+    vault[site] = {'first_name': site, 'last_name' : last_name, 'title': title, 'phone_number' : phone_number, 'email' : email}
+    
+def view_entry_list(vault: dict, fernet: Fernet, filtered_sites=None):
+       
+    if not vault:
+        return
+    
+    print("")
+    
+    sites = filtered_sites if filtered_sites is not None else list(vault.keys())
+    sites.sort(key=lambda site: vault[site]['last_name'])
+        
+    if not sites:
+        return
+
+    for i, site in enumerate(sites, start=1):
+        print("\33[34m"+f"{i}. "+"\33[0m"+ f"{vault[site]['last_name']}" + ", " + f"{vault[site]['first_name']}" + "\33[0m")
+        
+    print("\n")
+    
+    try: 
+        choice = int(input("Enter number (0 to cancel): "))
+        clear_screen()
+
+        if choice == 0:
+                return
+        selected_site = sites[choice - 1] 
+        print("\33[92mContact Information\33[0m\n")
+        print("\33[34mName:  \33[0m"+ vault[selected_site]['first_name'] + " " + vault[selected_site]['last_name'])
+        print("\33[34mTitle: \33[0m"+ vault[selected_site]['title'])
+        print("\33[34mPhone: \33[0m"+ vault[selected_site]['phone_number'])
+        print("\33[34mEmail: \33[0m"+ vault[selected_site]['email'])
+
+        c = input("\n\33[93mEnter to continue \33[0m")
+
+
+    except (ValueError, IndexError):
+        return
+               
+def delete_entry(vault: dict):
+    if not vault:
+        print("Vault is empty.")
+        return
+
+    sites = list(vault.keys())
+    sites.sort(key=lambda site: vault[site]['last_name'])
+    
+    print("\33[93mSelect a contact to delete:\33[0m\n")
+    for i, site in enumerate(sites, start=1):
+        
+        print("\33[34m"+f"{i}. "+"\33[0m"+ f"{vault[site]['last_name']}" + ", " + f"{vault[site]['first_name']}" + "\33[0m")
+       
+    try:
+        selection = int(input("\nEnter number (0 to cancel): "))
+        if selection == 0:
+            return
+        selected_site = sites[selection - 1]
+
+        del vault[selected_site]
+        print(f"✅ Contact deleted.")
+        
+    except (ValueError, IndexError):
+        print("Invalid selection.")
+    
+    time.sleep(1)
+
+def get_num_of_entries(vault: dict):
+    return len(vault)    
+
+def search_vault(vault: dict, fernet: Fernet):
+    term = input("\33[93mSearch: \33[0m").strip().lower()
+    matches = [site for site in vault if term in site.lower() or term in vault[site]['first_name'].lower() or term in vault[site]['first_name'].lower() or term in vault[site]['title'].lower() or term in vault[site]['phone_number'].lower() or term in vault[site]['email'].lower()]
+    clear_screen()
+    view_entry_list(vault, fernet, filtered_sites=matches)
+    
+def main():
+    
+    xtra_options = False
+       
+    while True:
+        master_password = '12345'
+        key = derive_key(master_password)
+        fernet = Fernet(key)
+        vault = load_vault(fernet)
+        num_of_entries = get_num_of_entries(vault)
+        clear_screen()
+        print("\33[93m================================\33[0m")
+        print("\33[34m            Contacts   \33[0m")
+        if xtra_options:
+            print("        Maintenance Mode ")
+        print("\33[93m================================\33[0m")
+        print("Total Contacts: " + str(num_of_entries))
+        
+        print("")
+
+        print("[\33[92mS\33[0m] Search")
+        print("[\33[92mV\33[0m] View Contacts")
+        if xtra_options:
+            print("[\33[92mA\33[0m] Add Contact")
+        if vault and xtra_options:
+            print("[\33[92mD\33[0m] Delete Contact")
+        if xtra_options:
+            print("[\33[92m?\33[0m] Normal Mode")
+        else:
+            print("[\33[92m?\33[0m] Maintenance Mode")
+        print("[\33[92mQ\33[0m] Quit\n")
+        
+        choice = input("> ").strip().lower()
+        clear_screen()
+        if choice == 'a' and xtra_options == True:
+            add_entry(vault)
+            save_vault(vault, fernet)
+        elif choice == 's' and vault:
+            search_vault(vault,fernet)
+        elif choice == 'v' and vault:
+            view_entry_list(vault, fernet)    
+        elif choice == 'd' and vault and xtra_options == True:
+            delete_entry(vault)
+            save_vault(vault, fernet)
+        elif choice == '?':
+            if xtra_options == True:
+                xtra_options = False
+            else:
+                xtra_options = True
+        elif choice == 'q':
+            save_vault(vault, fernet)
+            print("Contacts saved. Exiting.")
+            break
+        else:
+            print("Invalid option.")
+        
+if __name__ == "__main__":
+    main()
