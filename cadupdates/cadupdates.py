a4ff534f8643b195a5612005891eaa23436d476d
Krynnlord <richard.landon.miller@gmail.com>
2025-10-06 11:32:23 -0500

    Added better steps

--- cadupdates/cadupdates.py
+++ cadupdates/cadupdates.py
@@ -1,31 +1,39 @@
 import os
 
 message_step1 = "Patch database server (DB1)"
+message_step1b = "Reboot database server (DB1)"
+message_step1c = "Reboot database server (DB1DR)"
 message_step1_alt = "Patch database server (DB1DR)"
-message_step2 = "Failover database"
-message_step3a = "Stop MQ services (MQ1)"
-message_step3b = "Patch MQ1"
-message_step4a = "Stop MQ services (MQ2)"
-message_step4b = "Patch MQ2"
-message_step5a = "Stop MQ services (MQ3)"
-message_step5b = "Patch MQ3"
+message_step2 = "Reboot database server >> Failover database"
+message_step3a = "Stop MQ services (MQ1) >> patch server"
+message_step3b = "Reboot MQ1"
+message_step4a = "Stop MQ services (MQ2) >> patch server"
+message_step4b = "Reboot MQ2"
+message_step5a = "Stop MQ services (MQ3) >> patch server"
+message_step5b = "Reboot MQ3"
 message_step6 = "Patch ISM1"
+message_step6b = "Reboot ISM1"
 message_step7 = "Patch ISM2"
+message_step7b = "Reboot ISM2"
 message_step8 = "Patch WEBAPP1"
+message_step8b = "Reboot WEBAPP1"
 message_step9 = "Patch WEBAPP2"
+message_step9b = "Reboot WEBAPP2"
 message_step10a = "Patch IF1"
-message_step10b = "Check Xalt"
+message_step10b = "Reboot IF1 >> Check Xalt"
 message_step11 = "Patch IF2"
+message_step11b = "Reboot IF2"
 message_step12 = "Patch MQ4"
+message_step12b = "Reboot MQ4"
 message_step13 = "Patch MQ5"
+message_step13b = "Reboot MQ5"
 message_step14 = "Patch MQ6"
+message_step14b = "Reboot MQ6"
 message_step15 = "CAD Updates Complete!"
 
 
 
-def step_status(current_step, database, extraflag):
-    if extraflag == True:
-        show_current_step(current_step)
+def step_status(current_step, database):
     if current_step == 1 and database == 2:
         return f"Next Step: \33[92m{message_step1}\33[0m"
     elif current_step == 1 and database == 1:
@@ -44,34 +52,50 @@ def step_status(current_step, database, extraflag):
         return f"Next Step: \33[92m{message_step5a}\33[0m"
     elif current_step == 8:
         return f"Next Step: \33[92m{message_step5b}\33[0m"
-    elif current_step == 9 or current_step == 10:
+    elif current_step == 9:
         return f"Next Step: \33[92m{message_step6}\33[0m"
-    elif current_step == 11 or current_step == 12:
+    elif current_step == 10:
+        return f"Next Step: \33[92m{message_step6b}\33[0m"
+    elif current_step == 11:
         return f"Next Step: \33[92m{message_step7}\33[0m"
-    elif current_step == 13 or current_step == 14:
+    elif current_step == 12:
+        return f"Next Step: \33[92m{message_step7b}\33[0m"
+    elif current_step == 13:
         return f"Next Step: \33[92m{message_step8}\33[0m"
-    elif current_step == 15 or current_step == 16:
+    elif current_step == 14:
+        return f"Next Step: \33[92m{message_step8b}\33[0m"
+    elif current_step == 15:
         return f"Next Step: \33[92m{message_step9}\33[0m"   
+    elif current_step == 16:
+        return f"Next Step: \33[92m{message_step9b}\33[0m"
     elif current_step == 17:
         return f"Next Step: \33[92m{message_step10a}\33[0m"
     elif current_step == 18:
         return f"Next Step: \33[92m{message_step10b}\33[0m"
-    elif current_step == 19 or current_step == 20:
+    elif current_step == 19:
         return f"Next Step: \33[92m{message_step11}\33[0m"
+    elif current_step == 20:
+        return f"Next Step: \33[92m{message_step11b}\33[0m"
     elif current_step == 21 and database == 1:
         return f"2 Days Later: \33[92m{message_step1}\33[0m"
     elif current_step == 21 and database == 2:
         return f"2 Days Later: \33[92m{message_step1_alt}\33[0m"
     elif current_step == 22 and database == 1:
-        return f"Next Step: \33[92m{message_step1}\33[0m"
+        return f"Next Step: \33[92m{message_step1b}\33[0m"
     elif current_step == 22 and database == 2:
-        return f"Next Step: \33[92m{message_step1_alt}\33[0m"
-    elif current_step == 23 or current_step == 24:
+        return f"Next Step: \33[92m{message_step1c}\33[0m"
+    elif current_step == 23:
         return f"Next Step: \33[92m{message_step12}\33[0m"
-    elif current_step == 25 or current_step == 26:
+    elif current_step == 24:
+        return f"Next Step: \33[92m{message_step12b}\33[0m"
+    elif current_step == 25:
         return f"Next Step: \33[92m{message_step13}\33[0m"
-    elif current_step == 27 or current_step == 28:
+    elif current_step == 26:
+        return f"Next Step: \33[92m{message_step13b}\33[0m"
+    elif current_step == 27:
         return f"Next Step: \33[92m{message_step14}\33[0m"
+    elif current_step == 28:
+        return f"Next Step: \33[92m{message_step14b}\33[0m"
     elif current_step == 29:
         return f"\33[92m{message_step15}\33[0m"
     else:
@@ -195,7 +219,7 @@ def list_updates(database,current_step):
     
     print("")
 
-def print_extras():
+def print_extras(current_step):
     print("\33[93m==============================================\33[0m")
     print("Modules that are red and in Manual mode")
     print("are now normal and can be ignored.")
@@ -203,10 +227,12 @@ def print_extras():
     print ("Failover the database and check Xalt\n")
     print("\33[35mMQ Services Shutdown Procedure\33[0m")
     print("1. rabbitmq-upgrade drain")
-    print("2. rabbitmq-service stop")
+    print("2. rabbitmq-service stop\n")
+    show_current_step(current_step)
+    print("\33[93m==============================================\33[0m\n")
     
 def show_current_step(current_step):
-    print(f"\33[92m({current_step})\33[0m", end=" ")
+    print(f"\33[92mStep {current_step}\33[0m")
     
 def mark_next_step(current_step):
     if current_step != 29:
@@ -259,10 +285,9 @@ def main():
         print("\33[93m================================\33[0m")
         list_updates(database,current_step)
         if extraflag == True:
-            print_extras()
-        print("\33[93m==============================================\33[0m")
-        print(step_status(current_step,database, extraflag))
-        print("\33[93m==============================================\33[0m")
+            print_extras(current_step)
+        print(step_status(current_step,database))
+        print("")
         print("[\33[92m1\33[0m] Next Step ")
         print("[\33[92m2\33[0m] Backup Step ")
         print("[\33[92mC\33[0m] Change Primary Database", end="")
